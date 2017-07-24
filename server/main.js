135ceb1e93db66f130d2c7c9c772484059ba5f66
윤정현(Jeonghyun Yoon)/개발팀/SKP <jeonghyun.yoon@sk.com>
2017-07-24 15:45:50 +0900

    [lezhin] initial drop

---
+++ server/main.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const port = '8888';
+const uri = 'http://localhost:' + port;
+
+const app = express();
+
+app.use('/', express.static('src'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/banner', (req, res) => {
+  let device = req.query.device;
+  let dataUrl = "";
+  const count = req.query.count || undefined;
+
+  if( device !== 'desktop' && device !== 'mobile' ) {
+    device = 'mobile';
+  }
+
+  dataUrl = './db/' + device + '.json';
+
+  fs.readFile(dataUrl, 'utf8', ( error, data ) => {
+    let parsedData = JSON.parse( data );
+    if( count >= 0 && count <= parsedData.length ) {
+      parsedData = parsedData.slice(0, count);
+    }
+
+    res.writeHead(200, {'Content-Type': 'application/json' });
+    res.write(JSON.stringify(parsedData));
+    res.end();
+  });
+});
+
+console.log('> Starting dev server...')
+app.listen(port, () => {
+	console.log('Listening on port 8888');
+});
